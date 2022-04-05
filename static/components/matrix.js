377dd2dc317d081f1305a183e0ba818ce811ba7d
Matthew Hamilton <mwhamilton6@gmail.com>
2022-04-05 18:04:44 -0500

    Matrix is now an svg

--- static/components/matrix.js
+++ static/components/matrix.js
@@ -5,10 +5,10 @@ Vue.component('matrix', {
       if(+new Date() / 1000 < this.nextClick){
         return;
       }      
-      evt.path[0].style.backgroundColor = this.color;
+      evt.path[0].style.fill = this.color;
     },
     mouseleave: function(evt, element){
-      evt.path[0].style.backgroundColor = element?.color || null;
+      evt.path[0].style.fill = element?.color || '#ffffff';
     },
     addColor: function(i, j){
       if(+new Date() / 1000 < this.nextClick){
@@ -21,14 +21,27 @@ Vue.component('matrix', {
         timestamp: +new Date() / 1000,
       });
     },
+    translateRow(rowIndex){
+      return `translate(0, ${rowIndex * 40})`;
+    },
+    translateCol(colIndex){
+      return `translate(${colIndex * 40}, 0)`;
+    },    
   },
   template: `
-    <table style="border-collapse: collapse;margin:auto;margin-bottom: 40px;">
-      <tbody>
-        <tr v-for="row, i in pixels">
-          <td v-for="element, j in row" class="matrixSquare" @mouseenter="mouseenter" @mouseleave="mouseleave($event, element)" @click="addColor(i, j)" :style="{backgroundColor: element?.color}"></td>
-        </tr>
-      </tbody>
-    </table>
+    <div style="display: flex;justify-content: center;align-items: center; padding-top:60px">
+      <svg width=400 height=400>
+        <g v-for="row, i in pixels" :transform="translateRow(i)">
+          <g v-for="element, j in row" :transform="translateCol(j)">
+            <rect x=0 y=0 width=40 height=40
+              @mouseenter="mouseenter"
+              @mouseleave="mouseleave($event, element)"
+              @click="addColor(i, j)" 
+              :style="{fill: element?.color || '#ffffff', strokeWidth: '1px', stroke: '#000000'}" 
+            />
+          </g>
+        </g>
+      </svg>
+    </div>
   `
 })
